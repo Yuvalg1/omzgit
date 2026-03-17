e089a424df4330620270150d16088dc8a1a97557
Yuvalg1 <yuvalgoldvarg@gmail.com>
2026-03-17 21:31:12 +0200

    just content

--- popups/conflict/chunk/view.go
+++ popups/conflict/chunk/view.go
@@ -1,20 +1,5 @@
 package chunk
 
-import (
-	"omzgit/default/colors"
-	"omzgit/default/colors/bg"
-	"omzgit/default/colors/gray"
-
-	"github.com/charmbracelet/lipgloss"
-)
-
 func (m Model) View() string {
-	ourColor := colors.GetColor(m.ours, colors.Green, colors.Red)
-
-	return lipgloss.NewStyle().
-		Background(colors.GetColor(m.Active, bg.C[2], bg.C[0])).
-		Foreground(colors.GetColor(m.Conflict, ourColor, gray.C[2])).
-		Width(m.width).
-		Height(len(m.content)).
-		Render(m.content)
+	return m.content
 }
