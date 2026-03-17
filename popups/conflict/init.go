dc24c96ce697ca8bd30c76eb0ae17aae0e0f5f63
Yuvalg1 <yuvalgoldvarg@gmail.com>
2026-03-17 19:34:48 +0200

    refactored window size

--- popups/conflict/init.go
+++ popups/conflict/init.go
@@ -49,18 +49,18 @@ func getHeight(height int) int {
 	return height - 2
 }
 
-func (m Model) getOurAxis() int {
+func (m Model) getOurAxis() (int, int) {
 	if m.width > CUTOFF {
-		return m.width/2 + 1
+		return m.width/2 + 1, m.height - 1
 	}
-	return m.height / 2
+	return m.width, m.height / 2
 }
 
-func (m Model) getTheirAxis() int {
+func (m Model) getTheirAxis() (int, int) {
 	if m.width > CUTOFF {
-		return m.width/2 - (m.width+1)%2
+		return m.width/2 - (m.width+1)%2, m.height - 1
 	}
-	return m.height/2 - (m.height+1)%2 - 1
+	return m.width, m.height/2 - (m.height+1)%2 - 1
 }
 
 func (m Model) GetVisible() bool {
@@ -68,13 +68,11 @@ func (m Model) GetVisible() bool {
 }
 
 func (m *Model) getContent() {
-	if m.width > CUTOFF {
-		m.ours = content.InitialModel(m.getOurAxis(), m.height-1, true)
-		m.theirs = content.InitialModel(m.getTheirAxis(), m.height-1, false)
-	} else {
-		m.ours = content.InitialModel(m.width, m.getOurAxis(), true)
-		m.theirs = content.InitialModel(m.width, m.getTheirAxis(), false)
-	}
+	ourWidth, ourHeight := m.getOurAxis()
+	m.ours = content.InitialModel(ourWidth, ourHeight, true)
+
+	theirWidth, theirHeight := m.getTheirAxis()
+	m.theirs = content.InitialModel(theirWidth, theirHeight, false)
 
 	ourContent := ""
 	theirContent := ""
